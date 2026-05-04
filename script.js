e29870d8c656c80db7cdaa8595edb0ebebdd4266
monicadbarbosa <monicabarbosa.mdb@gmail.com>
2026-05-04 17:31:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -70,12 +70,9 @@ function updateHeader(){
         if(contactTop <= 120){
 
             header.classList.add("on-contact");
-            logo.src = "imagem/logo branco.png";
-
         } else{
-
             header.classList.remove("on-contact");
-            logo.src = "imagem/logo3.png";
+        
 
         }
 
